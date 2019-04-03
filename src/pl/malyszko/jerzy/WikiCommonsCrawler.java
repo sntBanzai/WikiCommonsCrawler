1b035a236fe9aa8ca1f955fb0d81e67fc8461c46
sntBanzai <jerzy.malyszko@pgn.corp>
2019-04-03 12:26:48 +0200

    mavenum

--- src/pl/malyszko/jerzy/WikiCommonsCrawler.java
+++ src/pl/malyszko/jerzy/WikiCommonsCrawler.java
@@ -1,30 +1,35 @@
-package projekt;
+package pl.malyszko.jerzy;
 
 import java.io.BufferedInputStream;
 import java.io.File;
-import java.io.FileWriter;
+import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.InputStream;
+import java.io.OutputStreamWriter;
 import java.io.StringWriter;
 import java.net.HttpURLConnection;
 import java.net.MalformedURLException;
 import java.net.ProtocolException;
 import java.net.URL;
+import java.nio.charset.Charset;
 import java.security.KeyManagementException;
 import java.security.NoSuchAlgorithmException;
 import java.security.cert.CertificateException;
-import java.util.Arrays;
 import java.util.Collections;
-import java.util.HashMap;
 import java.util.HashSet;
 import java.util.Iterator;
-import java.util.Map;
+import java.util.LinkedList;
+import java.util.List;
 import java.util.Set;
 import java.util.concurrent.ExecutionException;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
 import java.util.concurrent.ForkJoinPool;
 import java.util.concurrent.ForkJoinTask;
 import java.util.concurrent.RecursiveAction;
 import java.util.concurrent.TimeUnit;
+import java.util.concurrent.locks.ReadWriteLock;
+import java.util.concurrent.locks.ReentrantReadWriteLock;
 
 import javax.net.ssl.HostnameVerifier;
 import javax.net.ssl.HttpsURLConnection;
@@ -43,50 +48,102 @@ public class WikiCommonsCrawler {
 	private static final String $_PAGE_ID = "${pageID}";
 	private static final String $_CATEGORY_ID = "${categoryID}";
 	private static final String CATEGORYMEMBERS = "categorymembers";
-	private static final String BASE_ADDRESS = "https://commons.wikimedia.org/w/api.php?action=query&list=categorymembers&cmtype="
-			+ $_CM_TYPE + "&cmtitle=" + $_CATEGORY_ID + "&format=json&cmlimit=500";
+	private static final String BASE_ADDRESS = "https://commons.wikimedia.org/w/api.php?action=query&list=categorymembers&format=json&cmlimit=500&cmtype="
+			+ $_CM_TYPE + "&cmtitle=" + $_CATEGORY_ID;
 	private static final String LOCATION_COMMAND_LINK = "https://en.wikipedia.org/w/api.php?action=query&prop=info&pageids="
 			+ $_PAGE_ID + "&inprop=url&format=json";
 	private static final String CMCONTINUE = "cmcontinue";
 
-	static Map<String, Set<String>> outcome = Collections.synchronizedMap(new HashMap<>());
-	static FileWriter logWriter;
-	//static ForkJoinPool fjp = ForkJoinPool.commonPool();
+	static ForkJoinPool fjp;
+	static ReadWriteLock logLock = new ReentrantReadWriteLock();
+	static ReadWriteLock contentLock = new ReentrantReadWriteLock();
+	static List<String> logBuffer = Collections.synchronizedList(new LinkedList<>());
+	static List<String> contentBuffer = Collections.synchronizedList(new LinkedList<>());
 
 	static void log(String content) throws IOException {
-		synchronized (WikiCommonsCrawler.class) {
-			logWriter.write(content);
-		}
+		logLock.readLock().lock();
+		logBuffer.add(content);
+		logLock.readLock().unlock();
 	}
 
-	public static void main(String... args) throws  ExecutionException, IOException, KeyManagementException, NoSuchAlgorithmException, InterruptedException {
+	static void write(String content) throws IOException {
+		contentLock.readLock().lock();
+		contentBuffer.add(content);
+		contentLock.readLock().unlock();
+	}
 
-		try (FileWriter lw = new FileWriter(new File("log.output"))) {
-			logWriter = lw;
-
-			httpsPreparation();
-
-			ForkJoinTask<Void> forkJoinTask = new CategoryRecursiveAction("Category:Paintings").fork();
-
-			forkJoinTask.get();
-
-			ForkJoinPool.commonPool().awaitTermination(24, TimeUnit.HOURS);
-			
-		} finally {
-			logWriter.flush();
-			File output = new File("file.output");
-			try (FileWriter fw = new FileWriter(output)) {
-				outcome.forEach((key, val) -> {
-					try {
-						log((key + " " + Arrays.deepToString(val.toArray())));
-					} catch (IOException e) {
-						throw new RuntimeException(e);
+	public static void main(String... args) throws ExecutionException, IOException, KeyManagementException,
+			NoSuchAlgorithmException, InterruptedException {
+		String rootCategory = "Paintings";
+		int parallelism = Runtime.getRuntime().availableProcessors();
+		int timeout = 30;
+		TimeUnit tu = TimeUnit.HOURS;
+		if (args != null && args.length > 0) {
+			if (args.length > 1) {
+				parallelism = Integer.parseInt(args[1]);
+				if (args.length > 2) {
+					timeout = Integer.parseInt(args[2]);
+					if (args.length > 3) {
+						tu = TimeUnit.valueOf(args[3]);
 					}
-				});
-				fw.flush();
+				}
 			}
+
 		}
 
+		fjp = new ForkJoinPool(parallelism);
+		httpsPreparation();
+
+		ForkJoinTask<Void> forkJoinTask = fjp.submit(new CategoryRecursiveAction("Category:" + rootCategory)).fork();
+		Executors.newSingleThreadScheduledExecutor().scheduleAtFixedRate(new WriterFlushRecursiveAction(), 60, 60,
+				TimeUnit.SECONDS);
+
+		forkJoinTask.get();
+
+		fjp.awaitTermination(timeout, tu);
+
+	}
+
+	static class WriterFlushRecursiveAction implements Runnable {
+
+		static File logFile = new File("log" + System.currentTimeMillis() + ".output");
+		static File contentFile = new File("file" + System.currentTimeMillis() + ".output");
+
+		@Override
+		public void run() {
+			try {
+				System.out.println("flushing...");
+				try {
+					contentLock.writeLock().lock();
+					try (OutputStreamWriter lw = new OutputStreamWriter(new FileOutputStream(contentFile, true),
+							Charset.forName("UTF-8"))) {
+						for (String line : contentBuffer) {
+							lw.write(line + "\n");
+						}
+						contentBuffer.clear();
+					}
+
+				} finally {
+					contentLock.writeLock().unlock();
+				}
+				try {
+					logLock.writeLock().lock();
+					try (OutputStreamWriter fw = new OutputStreamWriter(new FileOutputStream(logFile, true),
+							Charset.forName("UTF-8"))) {
+						for (String line : logBuffer) {
+							fw.write(line + "\n");
+						}
+						logBuffer.clear();
+					}
+
+				} finally {
+					logLock.writeLock().unlock();
+				}
+				System.out.println("flushed!");
+			} catch (IOException e) {
+				throw new RuntimeException(e);
+			}
+		}
 	}
 
 	static class CategoryRecursiveAction extends RecursiveAction {
@@ -117,11 +174,8 @@ public class WikiCommonsCrawler {
 					Iterator<Object> iterator = jsonArray.iterator();
 					while (iterator.hasNext()) {
 						JSONObject arr = (JSONObject) iterator.next();
-						Set<String> newOne = new HashSet<>();
-						Set<String> cats = outcome.putIfAbsent(arr.getString("title"), newOne);
-						if (cats == null)
-							cats = newOne;
-						cats.add(category.replace("Category:", ""));
+						String content = arr.getString("title") + "@@@" + category.replace("Category:", "") + "\n";
+						write(content);
 					}
 
 					try {
@@ -144,7 +198,8 @@ public class WikiCommonsCrawler {
 					Set<String> subcats = new HashSet<>();
 					jsonArray.forEach(arr -> subcats.add(((JSONObject) arr).getString("title")));
 					for (String subc : subcats) {
-						new CategoryRecursiveAction(subc).fork();
+						ForkJoinTask<Void> submit = fjp.submit(new CategoryRecursiveAction(subc));
+						submit.fork();
 					}
 
 					try {
@@ -173,7 +228,7 @@ public class WikiCommonsCrawler {
 			URL url = new URL(address);
 			HttpURLConnection con = (HttpURLConnection) url.openConnection();
 			con.setRequestMethod("GET");
-			logWriter.write(address + "\n");
+			log(address.substring(address.lastIndexOf("&cmtype=")) + "\n");
 			return con;
 		}
 
